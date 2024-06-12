2569143f50ae30c4f3ebe14ed6759f8b88522ec7
Prthmesh <kapoorprathamesh02@gmail.com>
2024-06-12 17:45:41 +0530

    completed application

--- backend/models/Workout.js
+++ backend/models/Workout.js
@@ -12,7 +12,7 @@ const workOutSchema = new mongoose.Schema({
         },
         load: {
             type:Number,
-            reuired:true
+            required:true
         }
 
     },
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,34 @@
+import {useEffect} from 'react'
+import WorkoutDetails from "../components/workoutDetails.js"
+import WorkoutForm from '../components/workoutForm.js';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext.js';
+
+
 const Home = ()=> {
+    //const [workouts, setWorkouts] = useState(null);
+
+    const {workouts, dispatch} = useWorkoutsContext()
+    useEffect(()=>{
+       const fetchWorkouts = async()=>{
+          const response = await fetch('/api/workouts');
+
+          const json = await response.json();
+
+          if(response.ok){
+            //setWorkouts(json);
+            dispatch({type : 'SET_WORKOUTS', payload: json})
+          }
+       }
+       fetchWorkouts()
+    }, [dispatch]) 
  return (
     <div className="home">
-    <h2>
-        Home
-    </h2>
+    <div className="workouts">
+        {workouts && workouts.map((workout)=>(
+          <WorkoutDetails key={workout._id} workout={workout}/>
+        ))}
+    </div>
+    <WorkoutForm/>
     </div>
  )
 }
